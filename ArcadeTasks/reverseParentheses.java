73e3294d4a4541c5f455735239a5e4cf699d1698
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2018-03-27 15:20:02 +0200

    add reverseParentheses

---
+++ ArcadeTasks/reverseParentheses.java
@@ -0,0 +1,10 @@
+//SABRA CODE
+String reverseParentheses(String s) {
+    int r, l;
+    while((l = s.lastIndexOf('('))>=0){
+        r = s.indexOf(')',l);
+        s = s.replace(s.substring(l,r+1),new StringBuffer(s.substring(l+1,r)).reverse());
+
+    }
+    return s;
+}
