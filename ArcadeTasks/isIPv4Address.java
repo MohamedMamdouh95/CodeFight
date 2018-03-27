f237ee45bef4feb1df80a0131292cea9b1ab285b
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2018-03-27 15:17:51 +0200

    isIPv4Address

---
+++ ArcadeTasks/isIPv4Address.java
@@ -0,0 +1,3 @@
+boolean isIPv4Address(String inputString) {
+    return inputString.matches("([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+}
